6bbfb0d3431688a254c3e9b7420bb6bc99d0525a
tsingxiaohua <784948596@qq.com>
2018-08-24 10:44:49 +0800

    first commit

---
+++ BallanceDemo2-master/Assets/Script/BallanceAndMenu/BM_OptionControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BM_OptionControl : ButtonsMenu {
+    public GameObject OptionKeyBoard;
+    public GameObject OptionTouchBoard;
+    public GameObject OptionJoystick;
+    public override void OnButtinClicked(int ButtinId)
+    {
+        base.OnButtinClicked(ButtinId);
+        switch (ButtinId)
+        {
+            case 0:
+                GoToMenu(OptionKeyBoard);
+                break;
+            case 1:
+                GoToMenu(OptionTouchBoard);
+                break;
+            case 2:
+                GoToMenu(OptionJoystick);
+                break;
+            case 3:
+                BackToParent();
+                break;
+        }
+    }
+}
